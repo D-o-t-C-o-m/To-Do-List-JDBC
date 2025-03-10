b1c071eb8207ffde4ca98d235b981c31b31e1e98
valkuros@gmail.com <valkuros@gmail.com>
2025-03-09 22:10:45 -0400

    Cleaned up some excess code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-public static void main(String[] args) throws SQLException {
+public static void main(String[] args) {
 	Task task1 = new Task(55, "Learning Spring", false);
 	TaskRepository.create(task1);
 	TaskRepository.findById(task1);
@@ -17,7 +16,7 @@ public static void main(String[] args) throws SQLException {
 	}
 	System.out.println("=====================================");
 	Task task2 = new Task(1, "Learning Spring", true);
-	TaskRepository.update(task2);
+	TaskRepository.updateName(task2);
 	TaskRepository.findById(task2);
 	System.out.println("All Tasks:");
 	System.out.println("=====================================");
@@ -33,38 +32,5 @@ public static void main(String[] args) throws SQLException {
 	TaskRepository.DeleteAll();
 
 
-	//try {
-	//	var connection = DriverManager.getConnection("jdbc:h2:./todo;AUTO_SERVER=true", "sa", "");
-//		var createTableSQL = "create table if not exists TASK (id identity primary key, name varchar, completed boolean)";
-//		var statement = connection.createStatement();
-//		statement.execute(createTableSQL);
-//
-//		var insertQuery = "insert into TASK (name) values ('Leaning!')";
-//		statement.execute(insertQuery);
-//
-//		var insertStatement = "insert into Task (name) values (?)";
-//		var preparedStatement = connection.prepareStatement(insertStatement);
-//		preparedStatement.setString(1, "Learning Spring");
-//		preparedStatement.execute();
-//
-//		var updateCommand = "update TASK set name = ? where name = 'Leaning!'";
-//		preparedStatement = connection.prepareStatement(updateCommand);
-//		preparedStatement.setString(1, "Learning Jakarta EE!");
-//		preparedStatement.execute();
-//
-//		var deleteQuery = "delete from Task where name = 'Learning Jakarta EE!'";
-//		statement.execute(deleteQuery);
-//
-//		var selectAllQuery = "Select * from TASK";
-//		var resultSet = statement.executeQuery(selectAllQuery);
-//		while (resultSet.next()) {
-//			System.out.println("To Do Item: " + resultSet.getString("name"));
-//
-//
-
-	//} catch (Exception e) {
-//		System.out.println(e);
-//	}
-
-//	}
+//Add a quick UI to interact with the TaskRepository, I guess we could also set priorities as it would just be a column in the DB?
 }}
